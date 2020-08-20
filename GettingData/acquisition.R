00dc6616e24eab234aaaeefaea3e2ee1c52c9eee
Fernanda Bruno <februno@gmail.com>
2020-08-20 14:16:24 -0300

    First commit.Loading libraries.

---
+++ GettingData/acquisition.R
@@ -0,0 +1,14 @@
+####################
+##Data Acquisition##
+## August 20,2020 ##
+####################
+
+#Spotify package used
+#devtools::install_github('charlie86/spotifyr')
+
+#Packages used
+library(spotifyr)
+library(dplyr)
+library(tidyverse)
+
+
