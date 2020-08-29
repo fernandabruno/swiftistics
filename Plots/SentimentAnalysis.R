4445c8ef33d70af741d7eb866845d18da23f00d5
Fernanda Bruno <februno@gmail.com>
2020-08-29 00:48:24 -0300

    Adding other chord diagrams using Bing and Loughran lexicons.

--- Plots/SentimentAnalysis.R
+++ Plots/SentimentAnalysis.R
@@ -10,6 +10,7 @@ library(wordcloud)
 library(tidytext)
 library(stringr)
 library(textdata)
+library(circlize)
 
 #Read data
 lyrics<-read.csv2(file = "taytaylyrics.csv",stringsAsFactors = FALSE)
@@ -27,7 +28,7 @@ lyrics_dedup<-
 
 glimpse(lyrics_dedup)
 
-#Tokenizing lyrics 
+#Tokenizing lyrics  and applying NRC lexicon
 tidy_lyrics <- lyrics_dedup %>%
   select(album_name,text,track_name)%>%
   group_by(album_name,track_name)%>%
@@ -65,6 +66,7 @@ grid.col = c("Taylor Swift" = "grey",
 
 
 # Grouping songs on albuns to calculate intensity of emotions
+#Using nrc
 chord <- emotions %>%
   ungroup() %>%
   select(sentiment,album_name,n) %>%
@@ -80,8 +82,109 @@ circos.clear()
 circos.par(gap.after = c(rep(8, length(unique(chord[[1]])) - 1), 15,
                          rep(8, length(unique(chord[[2]])) - 1), 15))
 
-library(circlize)
+
 
 #Plot chord diagram
 chordDiagram(chord,grid.col = grid.col, transparency = .2)
 title("Taylor Swift Mood Over Her Albuns")
+
+#Using Bing Lexicon - Positivo x Negative
+emotions_bing<-
+  tidy_lyrics %>%
+  inner_join(get_sentiments("bing")) %>%
+  filter(!is.na(sentiment)) %>%
+  group_by(album_name,track_name)%>%
+  count(sentiment, sort = TRUE)%>%
+  arrange(desc(album_name))
+
+#Preparing chord diagram axis
+grid.col = c("Taylor Swift" = "grey", 
+             "Fearless" ="grey", 
+             "Speak Now" = "grey", 
+             "Red" = "grey", 
+             "1989" = "grey",
+             "reputation" = "grey",
+             "Lover" = "grey",
+             "folklore" = "grey",
+             "positive"="#000099",
+             "negative"="#CC0000")
+
+
+# Grouping songs on albuns to calculate intensity of emotions
+#Using nrc
+chord_bing <- emotions_bing %>%
+  ungroup() %>%
+  select(sentiment,album_name,n) %>%
+  group_by(album_name,sentiment)%>%
+  summarise(sentiment_sum = sum(n)) %>%
+  ungroup() %>%
+  select(sentiment,album_name,sentiment_sum)
+
+#Clear plot
+circos.clear()
+
+#Set the variables you want to show on the plot
+circos.par(gap.after = c(rep(8, length(unique(chord_bing[[1]])) - 1), 15,
+                         rep(8, length(unique(chord_bing[[2]])) - 1), 15))
+
+
+#Plot chord diagram
+chordDiagram(chord_bing,grid.col = grid.col, transparency = .2)
+title("Taylor Swift Mood Over Her Albuns - Bing")
+
+# using Afinn lexicon
+emotions_afinn<-
+  tidy_lyrics %>%
+  inner_join(get_sentiments("afinn")) %>%
+  filter(!is.na(value)) %>%
+  group_by(album_name,track_name)%>%
+  summarise(sum_value= sum(value))%>%
+  arrange(desc(album_name))
+
+# using loughran lexicon
+#Used on financial context
+emotions_loughran<-
+  tidy_lyrics %>%
+  inner_join(get_sentiments("loughran")) %>%
+  filter(!is.na(sentiment)) %>%
+  group_by(album_name,track_name)%>%
+  count(sentiment, sort = TRUE)%>%
+  arrange(desc(album_name))
+
+#Preparing chord diagram axis
+grid.col = c("Taylor Swift" = "grey", 
+             "Fearless" ="grey", 
+             "Speak Now" = "grey", 
+             "Red" = "grey", 
+             "1989" = "grey",
+             "reputation" = "grey",
+             "Lover" = "grey",
+             "folklore" = "grey",
+             "positive"="#000099",
+             "negative"="#CC0000",
+             "uncertainty" = "#E69F00"
+             )
+
+
+# Grouping songs on albuns to calculate intensity of emotions
+#Using nrc
+chord_loughran <- emotions_loughran %>%
+  ungroup() %>%
+  select(sentiment,album_name,n) %>%
+  group_by(album_name,sentiment)%>%
+  summarise(sentiment_sum = sum(n)) %>%
+  ungroup() %>%
+  select(sentiment,album_name,sentiment_sum)
+
+#Clear plot
+circos.clear()
+
+#Set the variables you want to show on the plot
+circos.par(gap.after = c(rep(8, length(unique(chord_loughran[[1]])) - 1), 15,
+                         rep(8, length(unique(chord_loughran[[2]])) - 1), 15))
+
+
+#Plot chord diagram
+chordDiagram(chord_loughran,grid.col = grid.col, transparency = .2)
+title("Taylor Swift Mood Over Her Albuns - Loughran")
+
